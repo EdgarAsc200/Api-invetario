2dc04cc6b20c8f349292dba7597cfc4c2523fc9d
Edgar Ascencio <em5212694@gmail.com>
2022-02-04 18:11:01 -0600

    Peticiones Get Con Filtro

--- models/get.model.php
+++ models/get.model.php
@@ -7,5 +7,14 @@ class GetModel{
         $query->execute();
         
         return $query->fetchall(PDO::FETCH_CLASS);
-    }
-}
+    } 
+
+    static public function getDataFilter($table,$LinkTo,$EqualTo){
+
+        $query = Conexion::connect()->prepare("SELECT * FROM $table where $LinkTo = :$LinkTo");
+        $query->bindParam(":".$LinkTo,$EqualTo,PDO::PARAM_STR);
+        $query->execute();
+
+        return $query->fetchall(PDO::FETCH_CLASS);
+    }   
+}    
